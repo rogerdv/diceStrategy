602c86583e0cd49caae51a4b2f8385b11f57ab28
Ghost <80626020+10ghost1@users.noreply.github.com>
2024-12-06 22:27:27 +0200

    Add files via upload

---
+++ characters/player.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+const DiceScenePath: String = "res://all_dice/dice.tscn"
+var dice_scene = preload(DiceScenePath)
+signal player_removed
+signal dice_added
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func set_dice():
+	var dice = dice_scene.instantiate()
+	dice.connect("inner_dice_removed", Callable(self, "_on_inner_dice_removed"))
+	dice._assign_character("player")
+	dice.scale = Vector2(0.4, 0.4)
+	add_child.call_deferred(dice)
+	
+func _on_inner_dice_removed():
+	emit_signal("player_removed")
+
+func reroll() -> void:
+	emit_signal("dice_added")
+	call_deferred("set_dice")
